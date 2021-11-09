7d9a42f153e720724e80470cefa3d90f8e833a28
vincentkaw <91302085+vincentkaw@users.noreply.github.com>
2021-11-10 02:43:07 +0800

    Navigation Bar

--- app/src/main/java/com/example/bmc208/MainActivity.java
+++ app/src/main/java/com/example/bmc208/MainActivity.java
@@ -1,10 +1,12 @@
 package com.example.bmc208;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.drawerlayout.widget.DrawerLayout;
 
 import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
