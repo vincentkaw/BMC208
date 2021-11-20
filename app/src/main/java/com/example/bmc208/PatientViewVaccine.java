76818298300d1815cfd191bb2f89b65f629ff511
chwung <weichung35@gmail.com>
2021-11-20 18:39:56 +0800

    layout of use case 3

---
+++ app/src/main/java/com/example/bmc208/PatientViewVaccine.java
@@ -0,0 +1,14 @@
+package com.example.bmc208;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class PatientViewVaccine extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_patient_view_vaccine);
+    }
+}
