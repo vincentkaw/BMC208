5bbdea6a0e3421368366208a3b54384a0d290b34
vincentkaw <91302085+vincentkaw@users.noreply.github.com>
2021-11-24 05:23:35 +0800

    sign up dialog

---
+++ app/src/main/java/com/example/bmc208/centerAdmin.java
@@ -0,0 +1,27 @@
+package com.example.bmc208;
+
+public class centerAdmin {
+    String center, addressAdmin;
+
+    public centerAdmin(String center, String addressAdmin) {
+        this.center = center;
+        this.addressAdmin = addressAdmin;
+    }
+
+    public String getCenter() {
+        return center;
+    }
+
+    public void setCenter(String center) {
+        this.center = center;
+    }
+
+    public String getAddressAdmin() {
+        return addressAdmin;
+    }
+
+    public void setAddressAdmin(String addressAdmin) {
+        this.addressAdmin = addressAdmin;
+    }
+
+}
